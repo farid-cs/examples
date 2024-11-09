b947f489bb97ea697ce93b5cc3b4061ad16da0e4
Farid <faridfaracli9@gmail.com>
2024-11-10 00:14:44 +0400

    evaluate math expressions

---
+++ eval.c
@@ -0,0 +1,40 @@
+/*
+ * the program evaluates simple mathematical expressions.
+ * expressions must contain only digits as operands and
+ * only following binary operators: + -.
+ *
+ * Sample expression: 1+2-3+4-5
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+long long
+eval(char *exp)
+{
+	long long result;
+
+	result = *exp++ - '0';
+	while (*exp) {
+		switch (*exp) {
+		case '+':
+			result += exp[1] - '0';
+			break;
+		case '-':
+			result -= exp[1] - '0';
+			break;
+		}
+		exp += 2;
+	}
+	return result;
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		fprintf(stderr, "usage: eval <expression>\n");
+		return 1;
+	}
+	printf("%ld\n", eval(argv[1]));
+	return 0;
+}
